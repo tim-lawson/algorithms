1e446e79a35aeb422364021e91fd3dc4540d8554
Tim Lawson <tim.lawson@bristol.ac.uk>
2024-05-31 10:00:46 +0100

    fix prod styles

--- src/components/collapse-trigger.tsx
+++ src/components/collapse-trigger.tsx
@@ -2,7 +2,6 @@ import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import classnames from "classnames";
 import React from "react";
-import { button, tooltip } from "@/classnames";
 import { Icons } from "@/components/icons";
 import { Algorithm } from "@/types";
 
@@ -26,7 +25,13 @@ export function CollapseTrigger({
           {/* https://github.com/radix-ui/primitives/discussions/560#discussioncomment-3477536 */}
           <span className="flex items-center justify-center">
             <CollapsiblePrimitive.Trigger asChild>
-              <button className={classnames(button)}>
+              <button
+                className={classnames(
+                  "flex shrink-0 h-6 w-6 items-center justify-center rounded-full select-none",
+                  "hover:bg-mauve3 text-mauve11 ring-mauve11",
+                  "focus:outline-none focus-visible:ring focus-visible:ring-opacity-75",
+                )}
+              >
                 <Icons.Expand value={isOpen} />
               </button>
             </CollapsiblePrimitive.Trigger>
@@ -34,7 +39,15 @@ export function CollapseTrigger({
         </TooltipPrimitive.Trigger>
         <TooltipPrimitive.Portal>
           <TooltipPrimitive.Content
-            className={classnames(tooltip)}
+            className={classnames(
+              "px-3 py-2 rounded text-xs leading-none select-none",
+              "data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade",
+              "data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade",
+              "data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade",
+              "data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade",
+              "shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]",
+              "will-change-[transform,opacity]",
+            )}
             side="left"
             sideOffset={6}
           >
--- src/components/learn-toggle.tsx
+++ src/components/learn-toggle.tsx
@@ -1,7 +1,6 @@
 import * as TogglePrimitive from "@radix-ui/react-toggle";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import classnames from "classnames";
-import { button, tooltip } from "@/classnames";
 import { Icons } from "@/components/icons";
 import { useLearned } from "@/page-context";
 
@@ -19,7 +18,9 @@ export function LearnToggle({ id }: { id: string }) {
               pressed={value}
               onPressedChange={toggle}
               className={classnames(
-                ...button,
+                "flex shrink-0 h-6 w-6 items-center justify-center rounded-full select-none",
+                "hover:bg-mauve3 text-mauve11 ring-mauve11",
+                "focus:outline-none focus-visible:ring focus-visible:ring-opacity-75",
                 "radix-state-on:bg-green5 radix-state-on:text-green11 radix-state-on:ring-green11",
                 "radix-state-off:bg-none radix-state-off:text-mauve11 radix-state-off:ring-mauve11",
               )}
@@ -31,7 +32,13 @@ export function LearnToggle({ id }: { id: string }) {
         <TooltipPrimitive.Portal>
           <TooltipPrimitive.Content
             className={classnames(
-              ...tooltip,
+              "px-3 py-2 rounded text-xs leading-none select-none",
+              "data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade",
+              "data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade",
+              "data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade",
+              "data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade",
+              "shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]",
+              "will-change-[transform,opacity]",
               value ? "bg-green5 text-green11" : "bg-mauve2 text-mauve11",
             )}
             side="left"
